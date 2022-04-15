9062eebca2251bf7fb4f71d21d4e71f257bb79bd
Serhii Chornobai <sergii.chornabai@sius.com>
2022-04-15 22:29:36 +0300

    wip

--- src/main.c
+++ src/main.c
@@ -52,18 +52,18 @@ int drawWindow(void){
     printf("%ld S\r\n",finish.tv_sec-start.tv_sec);
     DEV_Delay_ms(500);
 
-//    //Create a new image cache
-//    UBYTE *BlackImage;
-//    /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
-//    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0)? (EPD_7IN5_V2_WIDTH / 8 ): (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
-//    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-//        printf("Failed to apply for black memory...\r\n");
-//        return -1;
-//    }
-//    printf("Paint_NewImage\r\n");
-//    Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
+    //Create a new image cache
+    UBYTE *BlackImage;
+    /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0)? (EPD_7IN5_V2_WIDTH / 8 ): (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        return -1;
+    }
+    printf("Paint_NewImage\r\n");
+    Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
 
-    1.Select Image
+    // 1.Select Image
     printf("SelectImage:BlackImage\r\n");
     Paint_SelectImage(BlackImage);
     Paint_Clear(WHITE);
