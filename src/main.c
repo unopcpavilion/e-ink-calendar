482d477cd5b8817116c491272ef9c7b38a7feffa
Serhii Chornobai <sergii.chornabai@sius.com>
2022-04-15 19:49:54 +0300

    wip

---
+++ src/main.c
@@ -0,0 +1,32 @@
+﻿#include <stdlib.h>     //exit()
+#include <signal.h>     //signal()
+#include "EPD_Test.h"   //Examples
+
+void  Handler(int signo)
+{
+    //System Exit
+    printf("\r\nHandler:exit\r\n");
+    DEV_Module_Exit();
+
+    exit(0);
+}
+
+int main(void)
+{
+    // Exception handling:ctrl + c
+    signal(SIGINT, Handler);
+
+     EPD_7in5_V2_test();
+
+	// 	For Test
+    // if(DEV_Module_Init()!=0){
+        // return -1;
+    // }
+	// while(1) {
+	    // EPD_7in5b_V2_test();
+		// DEV_Delay_ms(10000);
+	// }
+	// DEV_Module_Exit();
+	//
+    return 0;
+}
